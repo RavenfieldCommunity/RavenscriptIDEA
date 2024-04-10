d4f91f7917cee363fe2a552447090ccee1a875b7
Leafx-code <82655578+Leafx-code@users.noreply.github.com>
2024-04-10 22:35:32 +0800

    Init.

---
+++ library/api/CommandManager.lua
@@ -0,0 +1,15 @@
+---@meta Ravenscript
+
+---@class CommandManager
+CommandManager = {
+	---@overload fun(commandName:string,arg1Type:string,arg2Type:string,global:bool,hostOnly:bool)
+	---@overload fun(commandName:string,arg1Type:string,arg2Type:string,arg3Type:string,global:bool,hostOnly:bool)
+	---@param commandName string
+	---@param arg1Type string
+	---@param global bool
+	---@param hostOnly bool
+	AddCustomCommand = function(commandName, arg1Type, arg2Type, global, hostOnly) end,
+	---@param name string
+	---@return Actor
+	GetActorByName = function(name) end,
+}
